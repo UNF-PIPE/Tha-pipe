e34cd520a958d752856bfd72fbb0d6875f14b42d
Niklas Malmqvist <n.malmqvist@gmail.com>
2012-05-13 15:58:55 +0200

    added get_gene to gbkHashOpt

--- gbkHashOps.pm
+++ gbkHashOps.pm
@@ -6,7 +6,7 @@ use File::Slurp;
 
 use Exporter;
 use base qw( Exporter );
-our @EXPORT_OK = qw(make_gbk_hash );
+our @EXPORT_OK = qw(make_gbk_hash get_gene);
 
 #Subroutine for making a hash of genbank annotation file	
 sub make_gbk_hash{	
@@ -56,7 +56,29 @@ sub make_gbk_hash{
 	return %gbkhash
 } #endbracket of subroutine make_gbk_hash
 
+sub get_gene{ 
+    #The soubroutine will retrieve a subseq (e.g. a gene) from a genome
+    #Arguments: 1 = path to genome, 2 = %gbkhash, 3 = protein_id, 4 = extension of startposition backwards (optional), 5 = extension of stop position (optional)
+    my $genome = Bio::SeqIO->new(-file => $_[0], -format => "fasta"); #Makes a SeqIO-object from the genome
+    my $gene;
 
+    while (my $seq = $genome->next_seq){
+	#Start and stop positions from %gbkhash
+	my $start = $_[1]{$_[2]}[0] - $_[3];
+	my $stop = $_[1]{$_[2]}[1] + $_[4];
+
+	#If gene is on complementary strand, take the reverse complement of the seq
+	if($_[1]{$_[2]}[2] == 0){
+	    $gene = $seq->subseq($start, $stop);
+	}
+	else{
+	    my $gene_obj = $seq->trunc($start, $stop);
+	    my $reversed = $gene_obj->revcom;
+	    $gene = $reversed->seq;
+	}
+    }
+    return $gene;
+}
 
 
 
