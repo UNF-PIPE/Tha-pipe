e830c8e20844d1ea0621e0ad14978b86b17a5b23
Simon Forsberg <simon@BioTunes-Server.student.uu.se>
2012-05-15 12:35:30 +0200

    -Moved the newest get_gene to gbkHashOps.pm and removed it from Main. Also removed make_gbk_hash from Main since it is in gbkHashOps.pm. -Made slight changes in t/gbkHashOps.t to match the new get_gene. -Added some test lines in Main.

--- Main.pl
+++ Main.pl
@@ -16,6 +16,7 @@ use Parallel::ForkManager;
 
 use OrthoMCLParser qw( parse_orthomcl_file );
 use findParalogs qw( findParalogs );
+use gbkHashOps qw( make_gbk_hash get_gene );
 
 #Get the parameters for ParserOrthoMCLgroups
 my $speciesPerLine;
@@ -38,7 +39,8 @@ while (my ($key,$value) = each %orthoHash) {
 	my $alignSequence = multipleAlign(\@{$value}, \%prot); 
 	#my $alignSequence = multipleAlign(\@{$value}, \%prot, \%INSERT NEW SEQUENCE HASH HERE <-----); 
 	if(my @gap_ids = findGaps($alignSequence,11)){
-		
+		print $gap_ids[0] . "\n";
+		print $alignSequence . "\n";
 	}
 
 	$pm->finish; # Terminates the child process
@@ -50,76 +52,6 @@ $pm->wait_all_children;
 
 #my %gbkhash = &make_gbk_hash("/home/data/NCBI-annotation/NC_008783.gbk");
 #print &get_gene("/home/data/NCBI-genomes/Bartonella_bacilliformis.fasta", \%gbkhash, $ARGV[0]);
-
-sub get_gene{
-    my ($genome_path,$orthoHash,$ID,$ext_start,$ext_stop) = @_;
-    #The soubroutine will retrieve a subseq (e.g. a gene) from a genome
-    #Arguments: 1 = path to genome, 2 = %gbkhash, 3 = protein_id, 4 = extension of startposition backwards (optional), 5 = extension of stop position (optional)
-    my $genome = Bio::SeqIO->new(-file => $genome_path, -format => "fasta"); #Makes a SeqIO-object from the genome
-    my $gene;
-
-    while (my $seq = $genome->next_seq){
-        #Start and stop positions from %gbkhash
-        my $start = $orthoHash->{$ID}[0] - $ext_start;
-        my $stop = $orthoHash->{$ID}[1] + $ext_stop;
-
-        #If gene is on complementary strand, take the reverse complement of the seq
-        if($orthoHash->{$ID}[2] == 0){
-            $gene = $seq->subseq($start, $stop);
-        }
-        else{
-            my $gene_obj = $seq->trunc($start, $stop);
-            my $reversed = $gene_obj->revcom;
-            $gene = $reversed->seq;
-        }
-    }
-    return $gene; 
-}
-
-
-#Subroutine for making a hash of genbank annotation file	
-sub make_gbk_hash{	
-	my $genbankfile = $_[0]; #downloadable from ftp://ftp.ncbi.nih.gov/genomes/Bacteria/Bartonella_bacilliformis_KC583_uid58533/
-	my $all_lines;
-	my %gbkhash;
-
-	my $ACID; #Accesion ID
-	my $start; #start location for ACID;
-	my $stop; #stop location for ACID;
-	my $complement; #values true or false
-	my $GI; # GI number
-	my $geneID; #GeneID
-
-	open GBFILE, $genbankfile or die $!;		
-	my @array = <GBFILE>;
-	chomp(@array);
-	$all_lines = join("", @array);
-	#print $all_lines;
-	my @array2= split(/\/translation/,$all_lines);
-	foreach(@array2){
-		if ($_ =~ m/CDS\s+(\d+)\.\.(\d+).\s+/){
-			#print "$1\t";
-			$start = $1;
-			$stop = $2;
-			$complement = 0;	
-		}
-		elsif ($_ =~ m/CDS\s+complement\((\d+)\.\.(\d+)\)\s+/) {
-			#print "$1 $2\t";
-			$start = $1;
-			$stop = $2;
-			$complement = 1;
-		}
-		if ($_ =~ m/\/protein_id=\"(.*)\"\s+\/db_xref=\"GI:(.*)\"\s+\/db_xref=\"GeneID:(.*)\"/) {
-			#print "$1\n";
-			$ACID = $1;			
-			$GI = $2;
-			$geneID = $3;
-		}
-		#Store in hash with AccessionID as key
-		$gbkhash{$ACID}=[$start, $stop, $complement, $GI, $geneID];
-	}
-	return %gbkhash
-} #endbracket of subroutine make_gbk_hash
 
 
 sub proteoms {
--- gbkHashOps.pm
+++ gbkHashOps.pm
@@ -25,62 +25,61 @@ sub make_gbk_hash{
 	my $geneID; #GeneID
 
 	foreach my $file (@files){
-	print $file."\n";
-	open GBFILE, $genbankDir.$file or die $!;		
-	my @array = <GBFILE>;
-	chomp(@array);
-	$all_lines = join("", @array);
-	#print $all_lines;
-	my @array2= split(/\/translation/,$all_lines);
-	foreach(@array2){
-		if ($_ =~ m/CDS\s+(\d+)\.\.(\d+).\s+/){
-			print "$1\t";
-			$start = $1;
-			$stop = $2;
-			$complement = 0;	
+		print $file."\n";
+		open GBFILE, $genbankDir.$file or die $!;		
+		my @array = <GBFILE>;
+		chomp(@array);
+		$all_lines = join("", @array);
+		#print $all_lines;
+		my @array2= split(/\/translation/,$all_lines);
+		foreach(@array2){
+			if ($_ =~ m/CDS\s+(\d+)\.\.(\d+).\s+/){
+				print "$1\t";
+				$start = $1;
+				$stop = $2;
+				$complement = 0;	
+			}
+			elsif ($_ =~ m/CDS\s+complement\((\d+)\.\.(\d+)\)\s+/) {
+				print "$1 $2\t";
+				$start = $1;
+				$stop = $2;
+				$complement = 1;
+			}
+			if ($_ =~ m/\/protein_id=\"(.*)\"\s+\/db_xref=\"GI:(.*)\"\s+\/db_xref=\"GeneID:(.*)\"/) {
+				print "$1\n";
+				$ACID = $1;			
+				$GI = $2;
+				$geneID = $3;
+			}
+			#Store in hash with AccessionID as key
+			$gbkhash{$ACID}=[$start, $stop, $complement, $GI, $geneID];
 		}
-		elsif ($_ =~ m/CDS\s+complement\((\d+)\.\.(\d+)\)\s+/) {
-			print "$1 $2\t";
-			$start = $1;
-			$stop = $2;
-			$complement = 1;
-		}
-		if ($_ =~ m/\/protein_id=\"(.*)\"\s+\/db_xref=\"GI:(.*)\"\s+\/db_xref=\"GeneID:(.*)\"/) {
-			print "$1\n";
-			$ACID = $1;			
-			$GI = $2;
-			$geneID = $3;
-		}
-		#Store in hash with AccessionID as key
-		$gbkhash{$ACID}=[$start, $stop, $complement, $GI, $geneID];
-	}
 	}
 	return %gbkhash
 } #endbracket of subroutine make_gbk_hash
 
-sub get_gene{ 
+sub get_gene{
+    my ($genome_path,$orthoHash,$ID,$ext_start,$ext_stop) = @_;
     #The soubroutine will retrieve a subseq (e.g. a gene) from a genome
     #Arguments: 1 = path to genome, 2 = %gbkhash, 3 = protein_id, 4 = extension of startposition backwards (optional), 5 = extension of stop position (optional)
-    my $genome = Bio::SeqIO->new(-file => $_[0], -format => "fasta"); #Makes a SeqIO-object from the genome
+    my $genome = Bio::SeqIO->new(-file => $genome_path, -format => "fasta"); #Makes a SeqIO-object from the genome
     my $gene;
 
     while (my $seq = $genome->next_seq){
-	#Start and stop positions from %gbkhash
-	my $start = $_[1]{$_[2]}[0] - $_[3];
-	my $stop = $_[1]{$_[2]}[1] + $_[4];
+        #Start and stop positions from %gbkhash
+        my $start = $orthoHash->{$ID}[0] - $ext_start;
+        my $stop = $orthoHash->{$ID}[1] + $ext_stop;
 
-	#If gene is on complementary strand, take the reverse complement of the seq
-	if($_[1]{$_[2]}[2] == 0){
-	    $gene = $seq->subseq($start, $stop);
-	}
-	else{
-	    my $gene_obj = $seq->trunc($start, $stop);
-	    my $reversed = $gene_obj->revcom;
-	    $gene = $reversed->seq;
-	}
+        #If gene is on complementary strand, take the reverse complement of the seq
+        if($orthoHash->{$ID}[2] == 0){
+            $gene = $seq->subseq($start, $stop);
+        }
+        else{
+            my $gene_obj = $seq->trunc($start, $stop);
+            my $reversed = $gene_obj->revcom;
+            $gene = $reversed->seq;
+        }
     }
     return $gene;
 }
-
-
-
+1;
