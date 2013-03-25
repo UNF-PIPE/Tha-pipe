9358481a4b6ac4b02f8d60dd85fa9df1b7b87332
Simon Forsberg <simon@hp.(none)>
2013-03-25 20:42:41 +0100

    The sub findAltStart are now called from Main. All the alignments are printed to separate files in a folder specified by the user. If an alignment contains paralogs, the filename will end with "_par".
    Some descriptive comments was added to Main. The data folder paths was changed from hard coded to user specified.

--- Main.pl
+++ Main.pl
@@ -13,37 +13,64 @@ use Bio::Tree::DistanceFactory;
 use Bio::Tree::TreeI;
 use Parallel::ForkManager;
 
-use findParalogs qw( findParalogs makeTree );
+use findParalogs qw( findParalogs );
 use HashRoutines qw( mkHash make_gbk_hash parse_orthomcl_file );
 use MapRoutines qw( get_gene );
 use multipleAlign qw(multipleAlign);
 use findAltStart qw( findAltStart findGaps );
 
 #Get the parameters for ParserOrthoMCLgroups
-my $speciesPerLine;
-my $proteinsPerSpecies;
-my $orthomcl_groups_file;
-GetOptions ("min|minSpeciesPerLine=s" => \$speciesPerLine, 'max|maxProteinsPerSpecies=s' => \$proteinsPerSpecies, "g|groupsfile=s" => \$orthomcl_groups_file);
+my ($speciesPerLine, $proteinsPerSpecies, $orthomcl_groups_file, $proteoms, $genomes, $annotations, $out);
+GetOptions ("min|minSpeciesPerLine=s" => \$speciesPerLine, 'max|maxProteinsPerSpecies=s' => \$proteinsPerSpecies, "g|groupsfile=s" => \$orthomcl_groups_file, "p|proteoms=s" => \$proteoms, "gs|genomes=s" => \$genomes, "a|annotations=s" => \$annotations, "o|outname=s" => \$out);
 
-#Test. The "wrap around" code goes here
+#Parse the orthomcl-file and create a hash with all ortholog groups that meet the criteria. 
 my %orthoHash = parse_orthomcl_file($orthomcl_groups_file, $speciesPerLine, $proteinsPerSpecies);
-my %prot = mkHash("t/test_data/proteome_data/NCBI-proteoms/");
-my %genome = mkHash("t/test_data/genome_data/NCBI-genomes/");
-my %annotation = make_gbk_hash("t/test_data/genome_data/NCBI-annotation/"); 
 
+#Make hashes for proteoms, genomes and annotation files. Keys are gene IDs, species and gene IDs respectively
+my %prot = mkHash($proteoms);
+my %genome = mkHash($genomes);
+my %annotation = make_gbk_hash($annotations);
+#my %prot = mkHash("t/test_data/proteome_data/NCBI-proteoms/");
+#my %genome = mkHash("t/test_data/genome_data/NCBI-genomes/");
+#my %annotation = make_gbk_hash("t/test_data/genome_data/NCBI-annotation/"); 
+
+#Get the number of processor cores and create a forkmanager object for multithreading usage.
 my $cores = `cat /proc/cpuinfo | grep 'processor'| wc -l`; #on Linux
 #my $cores = `sysctl -n hw.ncpu`; #on Mac
 my $pm = new Parallel::ForkManager($cores++);
+
 while (my ($key,$value) = each %orthoHash) {
 	my $pid = $pm->start and next;
-	my $alignedSequences = multipleAlign(\@{$value}, \%prot); 
-	my @gapSeqs = findGaps($alignedSequences, 13);
-    	my %extSeqs = findAltStart(\@gapSeqs, 13, \%annotation, \%genome);
-	$alignedSequences = multipleAlign(\@{$value}, \%prot, \%extSeqs); 
+	my $alignedSequences = multipleAlign(\@{$value}, \%prot);
 
-	my $tree = makeTree($alignedSequences);
-    	my $treeOut = $tree->as_text('newick');
-    	print $treeOut . "\n";
+        #Find sequences starting with more than n gaps. These will be passed on to finAltStart, which will try to find alternative start codons.
+#	my @gapSeqs = findGaps($alignedSequences, 13);
+#    	my %extSeqs = findAltStart(\@gapSeqs, 13, \%annotation, \%genome);
+        
+        #Re-align the extended sequences
+#	$alignedSequences = multipleAlign(\@{$value}, \%prot, \%extSeqs); 
+        
+        # Check for paralogs
+        my @Paralogs = findParalogs($alignedSequences);
+        my $outName;
+        if(@Paralogs){
+                #$outName = "/home/simon/Research/phylogeny_pipeline/results/130325/$key" . "_par.fasta";
+                $outName = $out . $key . "_par.fasta";
+                print "PARALOG!";
+                print join("\n", @Paralogs);
+        }
+        else{
+                $outName = $out . $key . ".fasta";
+                #$outName = "/home/simon/Research/phylogeny_pipeline/results/130325/$key" . ".fasta"; 
+        }
+        
+        #Write to file
+        open(my $outfile, ">", $outName);
+        print $outfile $alignedSequences;
+
+        #my $tree = makeTree($alignedSequences);
+        #my $treeOut = $tree->as_text('newick');
+        #print $treeOut . "\n";
 	$pm->finish; # Terminates the child process
 }
 $pm->wait_all_children;
